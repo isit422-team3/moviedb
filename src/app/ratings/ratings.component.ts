5ffcf89711a5149b2ed17801d774b35956deb0e3
Angela Vu <angela.vu@bellevuecollege.edu>
2018-11-13 08:06:19 -0800

    Fix Top Ratings Table
    - Table now subscribes for data directly from api service

--- src/app/ratings/ratings.component.ts
+++ src/app/ratings/ratings.component.ts
@@ -1,46 +1,27 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MovieObjectService } from '../services/movie-object.service';
 import { MatPaginator, MatTableDataSource } from '@angular/material';
-//import {SiteMovieModel} from '../services/db-client.service';
+import { ApiService } from '../services/api.service';
+//import { SiteMovieModel } from '../services/db-client.service';
 
 @Component({
   selector: 'app-ratings',
   templateUrl: './ratings.component.html',
   styleUrls: ['./ratings.component.scss']
 })
+
 export class RatingsComponent implements OnInit {
-  allRatings;
-  displayedColumns: string[] = ['title','rating'];
-  dataSource;
+  displayedColumns = ['title', 'rating', 'releaseDate'];
+  dataSource = new MatTableDataSource();
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-
-  constructor(private mos: MovieObjectService) { }
-
+  constructor(private apiService: ApiService) { }
+  
   ngOnInit() {
-    this.mos.CreateAllRatingsArray()
-    .then((data) => {this.allRatings = data
-    this.dataSource = new MatTableDataSource<Movie>(this.allRatings);
-    this.dataSource.paginator = this.paginator});
+    this.apiService.getAllRatings()
+    .subscribe((data) => {
+      this.dataSource.data = data['results'];
+    });
+    this.dataSource.paginator = this.paginator;
   }
-
 }
-
-export interface Movie {
-  title: string;
-  rating: number;
-}
-
-const RATINGS_DATA: Movie[] = [
-  {title: 'Dilwale Dulhania Le Jayenge', rating: 9.2},
-  {title: 'The Shawshank Redemption', rating: 8.6},
-  {title: 'The Godfather', rating: 8.6},
-  {title: 'Your Name.', rating: 8.5},
-  {title: 'Schindler\'s List', rating: 8.5},
-  {title: 'The Godfather: Part II', rating: 8.5},
-  {title: 'Spirited Away', rating: 8.5},
-  {title: 'Life Is Beautiful', rating: 8.4},
-  {title: 'The Green Mile', rating: 8.4},
-  {title: 'Pulp Fiction', rating: 8.4},
-];
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+//import { SiteMovieModel } from './db-client.service';
+//import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
