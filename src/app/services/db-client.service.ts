a6c0c11eaabe2008a5951a950bd961a2ff040fb2
benjaminvinicky <tyler40k@hotmail.com>
2018-11-19 14:38:55 -0800

    US0015: Additional client-side http requests and serverside methods.

--- src/app/services/db-client.service.ts
+++ src/app/services/db-client.service.ts
@@ -1,24 +1,55 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { User, DBMovie, Review } from '../models';
 
-@Injectable({
-  providedIn: 'root'
-})
+const api = '/api';
+
+
+@Injectable()
+
 //THIS SERVICE DEALS WITH TALKING TO OUR DB...
-export class DbClientService {
-  constructor() {
-  }
-}
 
-export class SiteMovieModel {
-  movie_id: number; 
-  title: string;
-  rating: number;
-  rating_count: number;
-  release: string;
-  genre: string;
-  description: string;
-  review_count: number;
-  background: string;
-  link: string;
+export class DbClientService {
+  constructor(private http: HttpClient) {}
+  
+  //MOVIE METHODS:
+  getMovies() {
+    return this.http.get<Array<DBMovie>>(`${api}/movies`)
+  }
+  deleteMovie(movie: DBMovie) {
+    return this.http.delete(`${api}/movie/${movie.movie_id}`);
+  }
+  addMovie(movie: DBMovie) {
+    return this.http.post<DBMovie>(`${api}/movie/`, movie);
+  }
+  updateMovie(movie: DBMovie) {
+    return this.http.put<DBMovie>(`${api}/movie/${movie.movie_id}`, movie);
+  }
+  ////////////////////////////////////////////////////////////////////////////
+  
+  //REVIEW METHODS: Note- if you want to get reviews then get the movie object and view its [reviews]
+  deleteReview(review: Review) {
+    return this.http.delete(`${api}/reviews/${review.movie_id, review.author_id}`);
+  }
+  addReview(review: Review) {
+    return this.http.post<Review>(`${api}/reviews/`, review);
+  }
+  updateReview(review: Review) {
+    return this.http.put<Review>(`${api}/reviews/${review.movie_id, review.author_id}`, review);
+  }
+  ////////////////////////////////////////////////////////////////////////////
+  
+  //USER METHODS
+  getUser(user: User) {
+    return this.http.get<Array<User>>(`${api}/users/${user.username}`)
+  }
+  deleteUser(user: User) {
+    return this.http.delete(`${api}/user/${user.username}`);
+  }
+  addUser(user: User) {
+    return this.http.post<User>(`${api}/user/`, user);
+  }
+  updateUser(user: User) {
+    return this.http.put<User>(`${api}/user/${user.username}`, user);
+  }
 }
