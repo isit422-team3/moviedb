77fc27b3375b094b15efacecc4a2d68efbaaafc0
Angela Vu <angela.vu@bellevuecollege.edu>
2018-11-21 03:50:47 -0800

    Add Scroller to Homepage and Fix CSS
    - Adds a movie scroller to the homepage to display top moves
    - Also fixes scroller CSS to arrow are easily noticed by the user

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MovieObjectService } from '../services/movie-object.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  topMovielist;
 
-  constructor() { }
+  constructor(private mos: MovieObjectService) { }
 
   ngOnInit() {
+    this.mos.CreateTopMovieArray()
+      .then((data) => this.topMovielist = data);
   }
 
 }
